0c23a23bcd3da02a114067b53fb45f823e4a4d8b
sami0567 <151971213+sami0567@users.noreply.github.com>
2023-11-25 17:44:13 -0500

    Add files via upload

---
+++ info.py
@@ -0,0 +1,75 @@
+
+#This File will contain the information to be displayed in your portfolio
+
+#CHANGE BELOW
+profile_picture = "clear.jpeg"
+about_me = "I'm Samiya Pathak, a first year CS major at Georgia Institute of Technology. I have an interest in Machine Learning and AI and I hope to gain CS realated work experience."
+
+
+#CHANGE BELOW (OPTIONAL)
+linkedin_image_url = "https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg"
+github_image_url = "https://cdn-icons-png.flaticon.com/256/25/25231.png"
+email_image_url = "https://logowik.com/content/uploads/images/513_email.jpg"
+
+#CHANGE BELOW
+my_linkedin_url = "https://www.linkedin.com/in/samiya-pathak-5395342a1/"
+my_github_url = "https://github.com/sami0567"
+my_email_address = "sami0567p@gmail.com"
+
+
+education_data ={
+    'Degree': 'Bachelor of Science in Computer Science',
+    'Institution': 'Georgia Institute of Technology',
+    'Location': 'Atlanta, GA',
+    'Graduation Date': 'May 2027',
+    'GPA': '4.0'
+}
+course_data = {
+    "code":["CS 1301", "CS 1331", "MATH 1552", "CS 2050"], 
+    "names":["Intro to CS", "Intro to OOP", "Integral Calculus", "Discrete Mathematics"], 
+    "semester_taken":["1st", "2nd", "2nd", "1st"],
+    "skills":["Data Structures: Lists, Dictionaries, Recursion, Abstraction, Debugging", "Object Oriented Programming in Java", "Definite and indefinite integrals, Techniques of integration, Improper integrals", "Formal Proofs and Logic, Algorithm Analysis "],
+    }
+experience_data = {
+    "Director of Web Design at Alpha Phi":(["In charge of the official Alpha Phi website.  Working on adding elements to the website that make it more user-friendly and aesthetically pleasing."], "websitee.jpeg"),
+    "Software Engineering Internship at Xyz":(["Spent my summer at Xyz in NYC as a Software Engineer Intern"],"amazon.jpeg"),
+    "GT Vertically Integrated Project: Brain Trauma Assessment Protocols":(["- Part of a research team that builds mobile applications for IOS and Android devices that collects and analyzes visual, auditory, and tactual input for people that suffered from Traumatic Brain Injuries.",
+                                                                                
+                                                                                "- Methods and technologies used in this project include Cloud Computing, User Interface Design, Multimedia Web Applications, Signal Processing, Databases, and Machine Learning."], "brain.jpeg"), 
+
+}
+
+projects_data = {
+    "Movie Portfolio": "Used Python, Movie APIs, and the Streamlit software to show a list of movies I love, like, and dislike based on genre with included plot, trailer, and movie poster",
+}
+
+programming_data = {
+    "Python": 90,
+    "HTML": 90,
+    "Java": 90,
+}
+
+#CHANGE BELOW (OPTIONAL)
+programming_icons = {
+    "Python": "🐍",
+    "HTML": "🖥️",
+    "Java": "🖲️",
+}
+spoken_icons = {"English": "🇺🇸",
+    "Hindi": "🇮🇳",
+    "Spanish":"🇪🇸"
+}
+
+#CHANGE BELOW
+spoken_data = {
+    "English": "Fluent",
+    "Hindi": "Fluent",
+    "Spanish": "Proficient",
+}
+leadership_data = {
+    "Teacher's Assistant": (["Assist the class CSXXXX"],"class.jpeg"),
+
+}
+activity_data={
+    "Research at GT": ["Assist Professor X with research on the subject X"]
+}
